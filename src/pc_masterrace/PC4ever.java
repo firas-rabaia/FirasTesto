cc402908ebafd17eb9519c229dab036266e51806
firas-0598968819 <firas-0598968819@192.168.1.111>
2020-02-09 00:09:24 +0200

    first message!

---
+++ src/pc_masterrace/PC4ever.java
@@ -0,0 +1,11 @@
+package pc_masterrace;
+
+public class PC4ever {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("PC master race");
+	}
+
+}
