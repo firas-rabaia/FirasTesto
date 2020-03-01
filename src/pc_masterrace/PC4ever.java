8e8324e8e7f6d528d9c8627f1a63df3ee0f31748
firas-0598968819 <firas-0598968819@192.168.1.103>
2020-03-01 20:31:53 +0200

    build.xml true

--- src/pc_masterrace/PC4ever.java
+++ src/pc_masterrace/PC4ever.java
@@ -1,23 +1,13 @@
 package pc_masterrace;
 
 public class PC4ever {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Consle for ever");
-		calc c= new calc();
-		int add=c.add(5, 6);
-		System.out.println(add);
-		
-	}
-}
-class calc {
+	
 	public int a,b;
 	
-	int add(int a,int b) {
+	public int add(int a,int b) {
 		return a+b;
 		
 	}
+	
 	
 }
--- src/pc_masterrace_test/testing.java
+++ src/pc_masterrace_test/testing.java
@@ -1,12 +1,37 @@
-import static org.junit.Assert.*;
+package pc_masterrace_test;
+import pc_masterrace.PC4ever;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.both;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.everyItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
-
 public class testing {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		PC4ever c = new PC4ever();
+		int result = c.add(5,1);
+		assertTrue(result == 6);
 	}
 
+	
+	
 }
