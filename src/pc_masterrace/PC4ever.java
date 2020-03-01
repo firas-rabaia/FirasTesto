4a97714ced2aae3dd0854dbc6c5fecdc6e293367
firas-0598968819 <firas-0598968819@192.168.1.103>
2020-03-01 17:16:37 +0200

    src with calc

--- src/pc_masterrace/PC4ever.java
+++ src/pc_masterrace/PC4ever.java
@@ -6,6 +6,18 @@ public class PC4ever {
 		// TODO Auto-generated method stub
 
 		System.out.println("Consle for ever");
+		calc c= new calc();
+		int add=c.add(5, 6);
+		System.out.println(add);
+		
 	}
-
+}
+class calc {
+	public int a,b;
+	
+	int add(int a,int b) {
+		return a+b;
+		
+	}
+	
 }
